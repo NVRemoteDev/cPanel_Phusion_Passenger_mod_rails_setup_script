8f2256dd7dc7e67e25def992d0fecedc1ccb04b9
Anthony Hill <hill.anthony@live.com>
2012-11-19 19:52:19 -0600

    initial commit working script

---
+++ rubypermissions.sh
@@ -0,0 +1,102 @@
+#!/bin/bash
+# Script will setup ruby rails permissions when running phusion passenger on cPanel
+
+echo "Setup your Ruby on Rails permissions on a cPanel server"
+echo "Tested on Phusion Passenger/mod_rails and cPanel 11"
+echo ""
+
+echo -e "This script will add 2 lines to the end of your Gemfile (gem 'therubyracer' and gem 'execjs', as well as run a bundle install/bundle package"
+echo -e "Please enter no at the next question if this is not OK"
+echo ""
+
+# Ensure user has setup a rails app already
+echo "Have you already created your rails application (rails new app) [yes/no]?"
+read answer
+if [ "$answer" == "yes" ]; then
+	echo "Continuing..."
+else
+	echo -e "run 'rails new your_app_name' in your desired directory (generally /home/username) before running this"
+	exit
+fi
+
+# Get paths and login
+echo -e "Enter your cPanel username (the username you use to login to cPanel): "
+read cpanelLogin
+
+echo -e "Enter your domain as it appears in cPanel (no http or www) i.e.: mydomain.com"
+read domain
+
+echo "Enter the FULL path to your Ruby application (no trailing slash) i.e.: /home/$cpanelLogin/railsapp"
+read appPath
+
+echo ""
+echo "Enter public_html folder name (will be created) - no leading slash (i.e.: enter myrailsapp for http://www.$domain/myrailsapp) - Leave blank if whole domain will be a rails app *CAUTION*"
+echo "No support for nested paths"
+read urlPath
+
+# Ensure correct information from user before continuing
+echo -e "cPanel Login: $cpanelLogin ; Rails Application Path: $appPath ; URL Path: /home/$cpanelLogin/public_html/$urlPath ; Domain: $domain"
+echo -e "Continue? [yes/no]:"
+read answer
+if [ "$answer" = "yes" ]; then
+	echo "Continuing..."
+else
+	exit
+fi
+
+echo ""
+
+# Add a symbolic link from public folder in rails app to URL path
+echo "Adding symbolic link from $appPath to /home/$cpanelLogin/public_html/$urlPath"
+
+ln -s $appPath/public/ /home/$cpanelLogin/public_html/$urlPath
+
+# Add apache include file for domain
+echo "Adding Apache include config"
+
+# Randomize conf name if urlPath is blank, else just assign randomName to urlPath
+if [ -z "$urlPath" ]; then
+	randomName=`tr -dc "[:alpha:]" < /dev/urandom | head -c 8`
+else
+	randomName=$urlPath
+fi
+
+echo "RailsBaseURI /$urlPath" >> /usr/local/apache/conf/userdata/std/2/$cpanelLogin/$domain/$randomName.conf
+echo "<Directory $appPath/public>" >> /usr/local/apache/conf/userdata/std/2/$cpanelLogin/$domain/$randomName.conf
+echo "	Allow from all" >> /usr/local/apache/conf/userdata/std/2/$cpanelLogin/$domain/$randomName.conf
+echo "	Options -MultiViews" >> /usr/local/apache/conf/userdata/std/2/$cpanelLogin/$domain/$randomName.conf
+echo "</Directory>" >> /usr/local/apache/conf/userdata/std/2/$cpanelLogin/$domain/$randomName.conf
+
+# Rebuild Apache confs
+echo "Rebuilding Apache configurations.  This may take a minute."
+
+/scripts/ensure_vhost_includes --user=$cpanelLogin
+/usr/local/cpanel/bin/apache_conf_distiller --update
+/usr/local/cpanel/bin/build_apache_conf
+service httpd restart
+
+# Set permissions
+echo "Setting directory permissions to $cpanelLogin else 500 error will happen"
+echo ""
+
+chown -h $cpanelLogin:$cpanelLogin $appPath
+chown -h $cpanelLogin:nobody /home/$cpanelLogin/$urlPath
+chown -hR $cpanelLogin:nobody $appPath/*
+
+# Add to gem file
+echo "Adding gem lines"
+echo \'gem therubyracer\' >> $appPath/Gemfile
+echo \'gem execjs\' >> $appPath/Gemfile
+
+# Installing Gems
+echo "Installing gems. This may take a minute"
+cd $appPath
+bundle install --path vendor
+echo ""
+bundle package
+
+echo "Restarting rails app..."
+touch $appPath/tmp/restart.txt
+
+echo ""
+echo "Finished!  Implement routing and controllers manually"
